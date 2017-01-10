ce45ec87f475d360315186a304a111ef1d78da23
Gustav Hansen <kusti8@gmail.com>
2017-01-10 15:46:19 -0500

    Move to v55

--- build-deb.sh
+++ build-deb.sh
@@ -3,7 +3,7 @@
 ## BUILDING SHOULD BE FOR chrome, chrome_sandbox and pdf targets:
 ## ninja -C out/Release chrome chrome_sandbox pdf
 
-OUT_FILES="catalog chromium-browser chrome_100_percent.pak chrome_200_percent.pak chrome_material_100_percent.pak chrome_material_200_percent.pak chrome-sandbox content_browser_manifest.json content_renderer_manifest.json content_resources.pak icudtl.dat keyboard_resources.pak libs libyuv.a locales natives_blob.bin pseudo_locales resources.pak snapshot_blob.bin xdg-mime xdg-settings"
+OUT_FILES="catalog chromium-browser chrome_100_percent.pak chrome_200_percent.pak chrome_material_100_percent.pak chrome_material_200_percent.pak chrome-sandbox content_browser_manifest.json content_renderer_manifest.json content_resources.pak icudtl.dat keyboard_resources.pak libs libyuv.a locales natives_blob.bin pseudo_locales resources.pak snapshot_blob.bin xdg-mime xdg-settings *.so *.pak nacl_helper_bootstrap nacl_helper_nonsfi nacl_irt_arm.nexe"
 
 if [ "$2" == "" ]; then
   echo "Usage: $0 deb_name version"
@@ -17,21 +17,26 @@ if [ "$EUID" -ne 0 ]
 fi
 
 OUT_DIR=$1
+LIB_DIR=$OUTDIR/usr/lib/arm-linux-gnueabihf
+LIBFFMPEG=libffmpeg_chrome.so.55
 
-wget https://github.com/kusti8/chromium-build/blob/master/chromium-browser_51.tar.gz?raw=true -O chromium.tar.gz
+wget https://github.com/kusti8/chromium-build/blob/master/chromium-browser_55.tar.gz?raw=true -O chromium.tar.gz
 tar xvf chromium.tar.gz
 mv chromium-browser_51.0.2704.91-0ubuntu0.14.04.1.6001 $OUT_DIR
 
 
-mv src/out/Release/chrome_sandbox src/out/Release/chrome-sandbox
-chown root:root src/out/Release/chrome-sandbox
-chmod 4755 src/out/Release/chrome-sandbox
-mv src/out/Release/chrome src/out/Release/chromium-browser
-mv src/out/Release/lib src/out/Release/libs
+mv src/out/armv6/chrome_sandbox src/out/armv6/chrome-sandbox
+chown root:root src/out/armv6/chrome-sandbox
+chmod 4755 src/out/armv6/chrome-sandbox
+mv src/out/armv6/chrome src/out/armv6/chromium-browser
+mv src/out/armv6/lib src/out/armv6/libs
 
 for FILE in $OUT_FILES; do
-  cp -r src/out/Release/$FILE $OUT_DIR/usr/lib/chromium-browser/
+  cp -r src/out/armv6/$FILE $OUT_DIR/usr/lib/chromium-browser/
 done
+
+cp src/out/armv6/$LIBFFMPEG $LIB_DIR/
+cp src/out/armv7/$LIBFFMPEG $LIB_DIR/neon/vfp/
 
 sed -i "/Version:/c\Version: $2" $OUT_DIR/DEBIAN/control
 
